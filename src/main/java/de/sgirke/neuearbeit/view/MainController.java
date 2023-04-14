e43128a41d7b4381874cc0064eaedf34218a01c6
TheUnimatrix <sebastian.girke@yahoo.de>
2023-04-14 22:26:13 +0200

    Umstellen auf Spring Boot

---
+++ src/main/java/de/sgirke/neuearbeit/view/MainController.java
@@ -0,0 +1,14 @@
+package de.sgirke.neuearbeit.view;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String index() {
+        return "start";
+    }
+
+}
