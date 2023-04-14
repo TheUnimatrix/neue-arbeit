aa5f76bd56a66090cace65e2794e24b07a686a75
TheUnimatrix <sebastian.girke@yahoo.de>
2023-04-14 22:31:15 +0200

    Verwenden von JQuery

--- src/main/webapp/resources/javascript/scripts.js
+++ src/main/webapp/resources/javascript/scripts.js
@@ -1,13 +1,25 @@
-function selectValueChange(selectList, form) {
-	selectStartYear = form.elements[form.name + ":startYear"];
-	selectEndYear = form.elements[form.name + ":endYear"];
-	
-	startYear = selectStartYear.value;
-	endYear = selectEndYear.value;
-	
-	if (selectList == selectStartYear && startYear > endYear) {
-		selectEndYear.value = startYear;
-	} else if (selectList == selectEndYear && startYear > endYear) {
-		selectStartYear.value = endYear;
+$(document).ready(() => {
+	$("select[name $= ':startYear']").change(selectValueChange);
+	$("select[name $= ':endYear']").change(selectValueChange);
+});
+
+function selectValueChange(event) {
+
+	// hole beide Auswahllisten
+	let $selectStartYear = $("select[name $= ':startYear']");
+	let $selectEndYear = $("select[name $= ':endYear']");
+
+	// hole die Komponente, welche das Event ausgelöst hat
+	let $eventTarget = $(event.target);
+
+	// lies die Jahreswerte aus
+	let startYear = parseInt($selectStartYear.val());
+	let endYear = parseInt($selectEndYear.val());
+
+	// überprüfe, welches Element das Event ausgelöst hat und ob die Jahreswerte ungültig geworden sind
+	if ($selectStartYear.is($eventTarget) && startYear > endYear) {
+		$selectEndYear.val(startYear);
+	} else if ($selectEndYear.is($eventTarget) && startYear > endYear) {
+		$selectStartYear.val(endYear);
 	}
 }
