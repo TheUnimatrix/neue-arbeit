af374d6132a5e92d7df103c22eb76e03c6e37bbb
TheUnimatrix <sebastian.girke@yahoo.de>
2023-04-20 00:12:50 +0200

    Dynamische Inhalte hinzugefügt

--- src/main/java/de/sgirke/neuearbeit/view/MainController.java
+++ src/main/java/de/sgirke/neuearbeit/view/MainController.java
@@ -1,7 +1,12 @@
 package de.sgirke.neuearbeit.view;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class MainController {
@@ -12,7 +17,15 @@ public class MainController {
     }
 
     @GetMapping("/working-days")
-    public String workingDaysPage() {
+    public String workingDaysPage(Model model) {
+        List<Integer> yearList = new ArrayList<>();
+        int year = LocalDate.now().getYear();
+
+        for (int i = 0; i < 10; i++) {
+            yearList.add(year++);
+        }
+
+        model.addAttribute("yearList", yearList);
         return "working-days";
     }
 
