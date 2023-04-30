e530365f090044f20a7e2d26e7a34df3556059e9
TheUnimatrix <sebastian.girke@yahoo.de>
2023-04-30 16:44:36 +0200

    Redundante public-Deklarierung entfernt

--- src/main/java/de/sgirke/neuearbeit/service/HolidayService.java
+++ src/main/java/de/sgirke/neuearbeit/service/HolidayService.java
@@ -18,41 +18,41 @@ public interface HolidayService {
 	 * @param endYear Endjahr des gewählten Zeitraums
 	 * @return Liste mit allen gesetzlichen Feiertagen in Thüringen
 	 */
-	public List<Holiday> getAllHolidays(int startYear, int endYear);
+	List<Holiday> getAllHolidays(int startYear, int endYear);
 	
 	/**
 	 * Getter-Methode zum Berechnen von Karfreitag.
 	 * @param year ausgewähltes Jahr zur Berechnung von Karfreitag
 	 * @return {@link Calendar}-Objekt mit den Daten zu Karfreitag
 	 */
-	public Calendar getGoodFriday(int year);
+	Calendar getGoodFriday(int year);
 	
 	/**
 	 * Getter-Methode zum Berechnen von Ostermontag.
 	 * @param year ausgewähltes Jahr zur Berechnung von Ostermontag
 	 * @return {@link Calendar}-Objekt mit den Daten zu Ostermontag
 	 */
-	public Calendar getEasterMonday(int year);
+	Calendar getEasterMonday(int year);
 	
 	/**
 	 * Getter-Methode zum Berechnen von Christi Himmelfahrt.
 	 * @param year ausgewähltes Jahr zur Berechnung von Christi Himmelfahrt
 	 * @return {@link Calendar}-Objekt mit den Daten zu Christi Himmelfahrt
 	 */
-	public Calendar getAscensionDay(int year);
+	Calendar getAscensionDay(int year);
 	
 	/**
 	 * Getter-Methode zum Berechnen von Pfingstmontag.
 	 * @param year ausgewähltes Jahr zur Berechnung von Pfingstmontag
 	 * @return {@link Calendar}-Objekt mit den Daten zu Pfingstmontag
 	 */
-	public Calendar getWhitMonday(int year);
+	Calendar getWhitMonday(int year);
 	
 	/**
 	 * Getter-Methode zum Berechnen von Ostersonntag nach der Gaußschen Osterformel.
 	 * @param year ausgewähltes Jahr zur Berechnung von Ostersonntag
 	 * @return {@link Calendar}-Objekt mit den Daten zu Ostersonntag
 	 */
-	public Calendar getEasterSunday(int year);
+	Calendar getEasterSunday(int year);
 	
 }
